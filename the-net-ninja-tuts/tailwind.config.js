4bffefd249058649cbfb9355cd9a50dc081d27b7
manhphm0997 <manh.pham@digicommercegroup.com>
2021-05-10 11:42:45 +0700

    lastest

--- the-net-ninja-tuts/tailwind.config.js
+++ the-net-ninja-tuts/tailwind.config.js
@@ -4,7 +4,19 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        primary: "#FF6363",
+        primary: {
+          DEFAULT: '#4C6FFF',
+          '50': '#FFFFFF',
+          '100': '#FFFFFF',
+          '200': '#E5EAFF',
+          '300': '#B2C1FF',
+          '400': '#7F98FF',
+          '500': '#4C6FFF',
+          '600': '#1946FF',
+          '700': '#002DE5',
+          '800': '#0023B2',
+          '900': '#00197F'
+        },
         secondary: {
           100: "#E2E2D5",
           200: "#888883",
